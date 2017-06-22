448d5f042f94ff0ce9382665d8f66aa79074beb1
Armin Fröhlich <mail@arminfroehlich.de>
2017-06-22 21:18:57 +0200

    preparations#2

--- .bin/edit_Openbox_config/screenConf.rb
+++ .bin/edit_Openbox_config/screenConf.rb
@@ -21,32 +21,34 @@ class ScreenConf
         ["Engrampa", "decor", "yes", "no" ],
         ["Baobab", "size/width", bigSc_width,smSc_width],
         ["Baobab", "size/height", bigSc_height, smSc_height],
-        ["Baobab", "decor", "yes", "no" ],
+        # ["Baobab", "decor", "yes", "no" ],
         ["libreoffice-startcenter", "decor", "yes", "no" ]
     ]
 
-    def applyChanges changes, doc
-        puts "blads"
+    def self.applyChanges changes, doc
         x, y = `xrandr`.scan(/current (\d+) x (\d+)/).flatten
         if x == '2560' && y == '1440'
             state = 2 # 2560x1440
         else
             state = 3
         end
-        puts changes.length
         changes.length.times do |i|
-            puts doc.root.elements["applications/application[@class='#{changes[i][0]}']/#{changes[i][1]}"].text = changes[i][state]
+            begin
             doc.root.elements["applications/application[@class='#{changes[i][0]}']/#{changes[i][1]}"].text = changes[i][state]
+            doc.root.elements["applications/application[@class='#{changes[i][0]}']/#{changes[i][1]}"].text = changes[i][state]
+            rescue
+                puts "Error in #{i}"
+            end
         end
     end
 
-    file = File.new("/home/armin/.config/openbox/rc.xml","r")
-    # file = File.new("/home/armin/.bin/edit_Openbox_config/rc.xml","r")
+    # # file = File.open("/home/armin/.config/openbox/rc.xml","r")
+    file = File.new("/home/armin/.bin/edit_Openbox_config/rc.xml","r")
     doc = Document.new(file)
-
+    
     applyChanges changes, doc
-    doc.write(File.open("/home/armin/.bin/edit_Openbox_config/rc.xml","w"), 2)
-    # doc.write(File.open("/home/armin/.config/openbox/rc.xml","w"), 2)
+    # doc.write(File.open("/home/armin/.bin/edit_Openbox_config/rc.xml","w"), 2)
+    doc.write(File.open("/home/armin/.config/openbox/rc.xml","w"), 2)
 
-    puts "bla"
+    puts "ScreenConf succeded"
 end
