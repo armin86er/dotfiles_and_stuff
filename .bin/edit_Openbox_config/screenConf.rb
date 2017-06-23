56366a226e184065a7951405a62fc80972f6abc4
Armin Fröhlich <mail@arminfroehlich.de>
2017-06-23 11:19:34 +0200

    inProcess

--- .bin/edit_Openbox_config/screenConf.rb
+++ .bin/edit_Openbox_config/screenConf.rb
@@ -7,25 +7,70 @@ class ScreenConf
     bigSc_height = "1145"
     smSc_width = "889"
     smSc_height = "694"
+    @i = 0
+    @add = ""
+    @arr = []
 
     changes2 = {"applications" => {
         "/application"=> {
-            "@[class=" => {"Xfce4-terminal" => { "]size/width" => "1360" },
-                        "name" => {  "size/width" => "1360"  } }
+            "@[class=" => {
+                "Xfce4-terminal" => { "]size/width" => "135560" },
+                "name" => {  "size/width" => "1360"  },
+                "anotherName" => {   "size/width" => "1360"   }}
         }
-        }
+    }
     }
     changes2.default = "nil"
 
-    def self.hashProcessing hashy
+    def self.hashProcessing hashy 
         begin
-        print hashy.keys[0]
-        hashProcessing hashy[ hashy.keys[0] ]
+            if hashy
+                hashy.each { |key, value| 
+                    # puts hashy.keys
+                    if value.is_a?(Hash)
+                        # print "Laenge: ", value.length,"  key: ", key, "\n"
+                        # print "  ", hashy.length, "   "
+                        # print key
+                        if hashy.length == 1
+                            @add = @add + key
+                        else
+                            # if value.is_a?(Hash)
+                                # (1..k)
+                                # print @add, key, "\n"
+                            # elsif value.is_a?(String)
+                            # end
+                        end
+                        hashProcessing hashy[ hashy.keys[@i] ]
+                    else
+                        # print key ,value, "\n"
+                    end
+                }
+            end
         rescue
-            puts 'resc'
+            puts "\nresc"
+            @i = @i + 1
         end
     end
     hashProcessing changes2
+
+    # def self.hashProcessing hashy
+    #     begin
+    #         puts '-------------'
+    #         puts hashy.length
+    #         # puts hashy.keys[0]
+    #         if hashy
+    #             hashy.each { |key, value|
+    #                 puts @i
+    #                 puts hashy.keys
+    #                 hashProcessing hashy[ hashy.keys[@i] ] }
+    #         end
+    #         # hashProcessing hashy[ hashy.keys[0] ]
+    #     rescue
+    #         puts "\nresc"
+    #         @i = @i + 1
+    #     end
+    # end
+    # hashProcessing changes2
 
     changes = [
         ["Xfce4-terminal", "size/width", "2100",smSc_width],
